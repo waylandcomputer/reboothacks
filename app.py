bff5c57f6815a74f901215f49704f973cb15b0b8
Shen0000 <bshen.324@gmail.com>
2021-02-09 17:45:13 -0500

    favicon fixed

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
+import os
 
 
 app = Flask(__name__)
@@ -11,3 +12,7 @@ def index():
 @app.route("/test")
 def newpage():
     return render_template("test.html")
+
+@app.route("/favicon.ico")
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, "static/assets"),"Project_Reboot_8.ico")
