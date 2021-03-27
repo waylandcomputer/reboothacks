cc3eddc9c54786c459c3c581460fa08438dc3fb5
ericx1e <ericxie6@gmail.com>
2021-03-27 12:35:29 -0400

    andy easter egg

--- app.py
+++ app.py
@@ -18,6 +18,9 @@ def challenge():
 def surprise():
     return render_template("easter-egg.html")
 
+@app.route("/andy")
+def andy():
+    return render_template("andy.html")
 
 @app.route("/robots.txt")
 def se1():
